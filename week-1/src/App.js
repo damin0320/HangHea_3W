36f1c8d71c37e913b019c041817a3f719f2b3517
damin <damin0320@gmail.com>
2022-09-30 17:48:18 +0900

    ADD : 제목과 입력창 폼 저장

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,11 +1,14 @@
 // src/App.js
 
 import "./App.css"; // 🔥 반드시 App.css 파일을 import 해줘야 합니다.
+import Header from "./components/header/Header";
+import Form from "./components/form/Form";
 
 function App() {
   return (
-    <div className="todos-container" id="todos">
-      안녕하세요
+    <div>
+        <Form />
+        <Header />
     </div>
   );
 }
