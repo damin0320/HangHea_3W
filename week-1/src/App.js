17a0b828fb1a63ab5bdc7a2d34fc7ed39912882e
damin <damin0320@gmail.com>
2022-09-30 16:23:26 +0900

    ADD : First Settings

---
+++ week-1/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+
+import "./App.css"; // 🔥 반드시 App.css 파일을 import 해줘야 합니다.
+
+function App() {
+  return (
+    <div className="todos-container" id="todos">
+      안녕하세요
+    </div>
+  );
+}
+
+export default App;
