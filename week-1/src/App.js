383ad5536f1227b0f4257776c850cf8c49ffc2f3
damin <damin0320@gmail.com>
2022-10-01 09:02:29 +0900

    ADD : 멘토님 강의 + todo 이어 붙이기

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,14 +1,32 @@
 // src/App.js
-
+import React, {useState} from 'react'
 import "./App.css"; // 🔥 반드시 App.css 파일을 import 해줘야 합니다.
-import Header from "./components/header/Header";
-import Form from "./components/form/Form";
+
 
 function App() {
+  const [inputValue, setInputValue] = useState("")
+  const [todos, setTodo] = useState([])
+
+  const onChange = e => {
+    setInputValue(e.target.value)
+  };
+
+  const onCreate = e =>{
+    setTodo(todos => [...todos, inputValue]);
+    setInputValue('')
+  }
+  
   return (
     <div>
-        <Form />
-        <Header />
+        <h1>나의 할 일</h1>
+        <input type="text" value={inputValue} onChange={onChange}/>
+
+        <button type="button" onClick={onCreate}>입력</button>
+        <div>할 일 내용
+        <ul>
+          {todos.map(ent => (<li key={ent}>{ent}</li>))}
+        </ul>
+        </div>
     </div>
   );
 }
