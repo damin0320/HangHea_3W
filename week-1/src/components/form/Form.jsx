80d3ef004b32341b0dc08b03a8d54bfdae1c5004
damin <damin0320@gmail.com>
2022-10-01 22:47:39 +0900

    ADD : 멘강의 통한 리스트 이어붙이기 완성

--- week-1/src/components/form/Form.jsx
+++ week-1/src/components/form/Form.jsx
@@ -1,11 +1,39 @@
-import { Component } from "react";
+import React, {useState} from 'react'
 
-export default class FormBar extends Component {
-	render() {
-		return (
-			<div>My Todo List
-        React
-      </div>
-		);
-	}
+function App() {
+	const [todoList, setTodoList] = useState([])
+	const [inputs, setInputs] = useState({
+		title : "",
+		content : ""
+	})
+
+const onChangeHandler = (e) => {
+setInputs({
+	...inputs,
+	[e.target.name] : e.target.value
+})}
+		
+
+const onSubmitHandler = () =>{
+	const newTodoList =  [...todoList, inputs];
+	setTodoList(newTodoList)
 }
+
+  return(
+    <div>
+      제목 <input type="text" name="title" onChange={onChangeHandler}/>
+			내용 <input type="text" name="content" onChange={onChangeHandler}/>
+			<button type="button" onClick={onSubmitHandler}>추가하기</button>
+			<ul>
+					{todoList.map((item) =>{
+						return <li key={item}>{item.title} {item.content}</li>
+					})}
+				</ul>
+
+
+    </div>
+  )
+
+}
+
+export default App;
---
+++ week-1/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+
+  return(
+    <div>
+      <div>My Todo List</div>
+      <div>React</div>
+    </div>
+  )
+}
+
+export default App;
